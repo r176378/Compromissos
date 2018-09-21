3ee5c036b029298418a5f370c2bf5d578e7b7d54
Ranieri <r176378@dac.unicamp.br>
2018-09-21 17:14:31 -0300

    Initial change

---
+++ app/src/main/java/br/unicamp/ft/r176378_f171258/compromissos/Compromisso.java
@@ -0,0 +1,31 @@
+package br.unicamp.ft.r176378_f171258.compromissos;
+
+public class Compromisso {
+    String title;
+    String date;
+    String time;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
---
+++ app/src/main/java/br/unicamp/ft/r176378_f171258/compromissos/CompromissoCollection.java
@@ -0,0 +1,7 @@
+package br.unicamp.ft.r176378_f171258.compromissos;
+
+import java.util.ArrayList;
+
+public class CompromissoCollection {
+    ArrayList<Compromisso> ComprimissoList= new ArrayList<>();
+}
